01287164f9f7a726b65bf59ef8c1910880e0f279
RoseThorn10 <lrroman@comcast.net>
2022-10-28 10:53:10 -0500

    Bug fixes

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Contact Cards'
+        title: 'JATE'
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
@@ -29,9 +29,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Edit text the old-fashioned way!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
